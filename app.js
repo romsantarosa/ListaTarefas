e4309a3f536d5877f3b170f4c929f9e619b5422f
RomSantaRosa <mourarom@gmail.com>
2020-04-01 21:48:49 +0100

    whats express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express')
+
+const app = express() //todos os metodos disponiveis no servidor atraves da constante app
+
+app.get('/',(req, res) => {
+    res.send('<h1>Minhas listas de tarefas</h1>') // setando as rotas
+})
+
+app.listen(3000, () => {
+    console.log('O servidor foi iniciado!') // startando server
+
+})
